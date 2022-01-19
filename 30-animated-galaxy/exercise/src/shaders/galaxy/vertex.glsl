fee0964ef7a33f2ecfc5a581ea7e2b78e3288c14
momentchan <mingjyunhung@gmail.com>
2022-01-19 10:23:03 +0900

    add animated galaxy

---
+++ 30-animated-galaxy/exercise/src/shaders/galaxy/vertex.glsl
@@ -0,0 +1,39 @@
+uniform float uTime;
+ uniform float uSize;
+ attribute float aScale;
+ attribute vec3 aRandomness;
+ varying vec3 vColor;
+
+ void main() {
+    /**
+    * Position
+    */
+    vec4 modelPosition = modelMatrix * vec4(position,1.0);
+
+    // Spin
+    float angle = atan(modelPosition.x, modelPosition.z);
+    float distanceToCenter = length(modelPosition.xz);
+    float angleOffset = (1.0 / distanceToCenter) * uTime * 0.2;
+    angle += angleOffset;
+
+    modelPosition.x = cos(angle) * distanceToCenter;
+    modelPosition.z = sin(angle) * distanceToCenter;
+
+    // Randomness
+    modelPosition.xyz += aRandomness;
+
+    vec4 veiwPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * veiwPosition;
+    gl_Position = projectedPosition;
+
+    /**
+    * Size
+    */
+    gl_PointSize = uSize * aScale;
+    gl_PointSize *= ( 1.0 / - veiwPosition.z );
+    
+    /**
+    * Color
+    */
+    vColor = color;
+}
