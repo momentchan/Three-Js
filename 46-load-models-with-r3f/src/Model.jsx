4601b197917db481effb8ddb9406334370075308
momentchan <mingjyunhung@gmail.com>
2024-01-08 19:02:43 +0800

    add model loader

---
+++ 46-load-models-with-r3f/src/Model.jsx
@@ -0,0 +1,30 @@
+import { Clone, useGLTF } from '@react-three/drei'
+
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+
+export default function Model() {
+    // // Method1
+    // const model = useLoader(
+    //     GLTFLoader,
+    //     './hamburger.glb',
+    //     (loader) => {
+    //         const dracoLoader = new DRACOLoader()
+    //         dracoLoader.setDecoderPath('./draco/')
+    //         loader.setDRACOLoader(dracoLoader)
+    //     }
+    //  )
+
+    // Method 2
+    const model = useGLTF('./hamburger-draco.glb')
+
+    return <>
+        <Clone object={model.scene} scale={0.35} position-x={-4} />
+        <Clone object={model.scene} scale={0.35} position-x={0} />
+        <Clone object={model.scene} scale={0.35} position-x={4} />
+    </>
+
+}
+
+useGLTF.preload('./hamburger-draco.glb')
