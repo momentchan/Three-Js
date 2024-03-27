849f32808ad17843f008b2534361e8afb75437a6
momentchan <mingjyunhung@gmail.com>
2024-03-27 17:13:09 +0800

    add sliced model shader

---
+++ 43-sliced-model-shader/src/shaders/sliced/fragment.glsl
@@ -0,0 +1,16 @@
+varying vec3 vPosition;
+uniform float uSliceStart;
+uniform float uSliceArc;
+
+
+void main()
+{
+    float angle = atan(vPosition.y, vPosition.x);
+    angle -= uSliceStart;
+    angle = mod(angle, PI * 2.0);
+
+    if(angle > 0.0 && angle < uSliceArc)
+        discard;
+
+    float csm_Slice; // replacement
+}
